44ae0593fc27ec5a680db5edc1afea39e72fb509
timonkus <timonkus@gmail.com>
2023-10-26 23:14:55 +0300

    hf1

--- components/Section/SectionHappyClients.vue
+++ components/Section/SectionHappyClients.vue
@@ -1,16 +1,16 @@
 <template>
     <section class="global">
       <div class="container">
-        <div class="flex gap-16 flex-col-reverse lg:flex-row items-center">
+        <div class="flex gap-16 lg:pb-[200px] pb-[0px] flex-col-reverse lg:flex-row items-center">
           <img src="/img/global/map.png" alt="">
-          <div class="flex flex-col">
-            <img class="mb-16" src="/img/global/logo.png" alt="">
-            <div class="text-7xl font-medium mb-16">
+          <div class="flex flex-col z-0 lg:mb-0 -mb-[368px] ">
+            <img class="mb-16 translate-x-86" src="/img/global/logo.png" alt="">
+            <div class="font-medium mb-16 lg:text-[64px] sm:text-[46px] text-[28px] lg:leading-[64px] sm:leading-[50px] leading-[30px]">
               Счастливые клиенты <br/>
               уже <span class="text-[#047450]">в 37 регионах</span> России
             </div>
-            <div class="text-7xl font-medium text-[#047450]">5000+</div>
-            <span>Объектов</span>
+            <div class="text-7xl font-medium text-[#047450] sm:text-[80px] text-[46px] sm:leading-[80px] leading-[50px]">5000+</div>
+            <span class="text-[20px] leading-[26px]">Объектов</span>
           </div>
         </div>
       </div>
