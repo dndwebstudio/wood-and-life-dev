131cf5663bcbcdc245c4a747ca012e5655ae5f62
timonkus <timonkus@gmail.com>
2023-10-26 23:38:04 +0300

    hf2

--- components/Section/SectionHappyClients.vue
+++ components/Section/SectionHappyClients.vue
@@ -1,10 +1,10 @@
 <template>
     <section class="global">
-      <div class="container">
+      <div class="container overflow-hidden">
         <div class="flex gap-16 lg:pb-[200px] pb-[0px] flex-col-reverse lg:flex-row items-center">
-          <img src="/img/global/map.png" alt="">
+          <img class="sm:translate-x-[224px] sm:-translate-y-[109px] lg:translate-x-0 lg:-translate-y-0 translate-x-[67px] translate-y-[31px]" src="/img/global/map.png" alt="">
           <div class="flex flex-col z-0 lg:mb-0 -mb-[368px] ">
-            <img class="mb-16 translate-x-86" src="/img/global/logo.png" alt="">
+            <img class="mb-[20px]" src="/img/global/logo.png" alt="">
             <div class="font-medium mb-16 lg:text-[64px] sm:text-[46px] text-[28px] lg:leading-[64px] sm:leading-[50px] leading-[30px]">
               Счастливые клиенты <br/>
               уже <span class="text-[#047450]">в 37 регионах</span> России
