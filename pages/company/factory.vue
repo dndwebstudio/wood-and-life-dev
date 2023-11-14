4dd7a6ee348da786655db25101e9c62ba6a9015b
Pavel Kezlov <dev@dndstudio.ru>
2023-11-14 03:17:56 +0300

    factory

--- pages/company/factory.vue
+++ pages/company/factory.vue
@@ -14,24 +14,54 @@
     <SectionAdvantages :data="advantagesData1" />
     <SectionFeatures :data="featuresData" />
     <SectionHousesInterior :data="slides" />
-    <SectionInfoSimple class="techData2" id="technologies" :data="techData2">
-      <template #desc>
-        <p class="lg:pt-[64px] pt-[0]" style="">
-          <b class="text-[32px] leading-9 font-medium lg:text-[32px]">Закупаем сырье большими партиями и в<br/>
-             межсезонье, что позволяет оптимизировать<br/>
-             себестоимость готовой продукции</b><div class="lg:pt-[48px] pt-[0]" style="font-size: 24px; padding-bottom: 24px;">Повышение эффективности использования оборудования       </div></p>
-            <div class="flex gap-4 text-[16px] leading-[19px]">
-              <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
-                <path d="M2 4.83447L2 6.83447C2 9.04361 3.79086 10.8345 6 10.8345L17 10.8345" stroke="#838383"/>
-                <path d="M12 5.83447L17 10.8345L12 15.8345" stroke="#838383"/>
-                </svg>
-              </div>
-              <div style="width: 100%;"><p class="lg:text-[16px] ">Использование специализированных инструментов и оборудования значительно ускоряет <br>
-                      процесс обработки древесины и снижает затраты на труд</p></div>
+    <section class="pt-[40px] md:pt-24 lg:pt-48 bg-[#fff] mb-[40px] md:mb-[120px]">
+      <div class="container">
+        <div class="mb-[40px] md:mb-[64px] uppercase text-[14px] 2xl:text-[18px] leading-[120%] font-bold text-[#838383]">Объемы производства</div>
+        <div class="flex gap-12 flex-col lg:flex-row">
+          <div class="w-full lg:w-1/2 flex flex-col justify-center">
+            <div class="text-[#009767] text-[28px] md:text-[46px] xl:text-[64px] mb-[32px] xl:mb-[64px] font-medium leading-[100%]">
+              Оптимизировали расходы<span class="text-black">, чтобы сделать продукцию еще более доступной для каждого</span>
             </div>
-        </template>
-    </SectionInfoSimple>
+            <div class="text-[20px] md:text-[32px] font-medium leading-[110%] mb-[24px] xl:mb-[48px]">
+              Закупаем сырье большими партиями и в межсезонье, что позволяет оптимизировать себестоимость готовой продукции
+            </div>
+            <div class="flex flex-col gap-[24px]">
+              <div class="text-[#292929] text-[18px] md:text-[24px] leading-[120%]">
+                Повышение эффективности использования оборудования
+              </div>
+              <div class="flex text-[#292929] text-[24px] gap-4">
+                <svg class="shrink-0" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M2 4.83447L2 6.83447C2 9.04361 3.79086 10.8345 6 10.8345L17 10.8345" stroke="#838383"/>
+                  <path d="M12 5.83447L17 10.8345L12 15.8345" stroke="#838383"/>
+                </svg>
+                <div class="text-[16px] text-[#838383] leading-[120%]">Использование специализированных инструментов и оборудования значительно ускоряет процесс обработки древесины и снижает затраты на труд</div>
+              </div>
+            </div>
+          </div>
+          <div class="w-full lg:w-1/2">
+            <img class="max-w-[776px] max-h-[776px] w-full h-full object-cover aspect-square" src="/public/img/factory/img.png" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+<!--    <SectionInfoSimple class="techData2" id="technologies" :data="techData2">-->
+<!--      <template #desc>-->
+<!--        <p class="lg:pt-[64px] pt-[0]" style="">-->
+<!--          <b class="text-[32px] leading-9 font-medium lg:text-[32px]">Закупаем сырье большими партиями и в<br/>-->
+<!--             межсезонье, что позволяет оптимизировать<br/>-->
+<!--             себестоимость готовой продукции</b><div class="lg:pt-[48px] pt-[0]" style="font-size: 24px; padding-bottom: 24px;">Повышение эффективности использования оборудования       </div></p>-->
+<!--            <div class="flex gap-4 text-[16px] leading-[19px]">-->
+<!--              <div>-->
+<!--                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">-->
+<!--                <path d="M2 4.83447L2 6.83447C2 9.04361 3.79086 10.8345 6 10.8345L17 10.8345" stroke="#838383"/>-->
+<!--                <path d="M12 5.83447L17 10.8345L12 15.8345" stroke="#838383"/>-->
+<!--                </svg>-->
+<!--              </div>-->
+<!--              <div style="width: 100%;"><p class="lg:text-[16px] ">Использование специализированных инструментов и оборудования значительно ускоряет <br>-->
+<!--                      процесс обработки древесины и снижает затраты на труд</p></div>-->
+<!--            </div>-->
+<!--        </template>-->
+<!--    </SectionInfoSimple>-->
     <SectionChoose :data="choose" />
   </div>
 </template>
